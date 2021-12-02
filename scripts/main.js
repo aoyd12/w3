69e36f92465f59bebdfe318e4ccf55abeb47fb7c
aoyd12 <95006309+aoyd12@users.noreply.github.com>
2021-12-02 11:25:53 +0900

    サンプルページの動作確認変更

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 // Image switcher code
 
-let myImage = document.querySelector('img');
+let myImage = document.querySelector('img');//変数宣言 letを使う
+//documentは組み込みのオブジェクト、
+//ブラウザが表示しているページ、DOMとも呼ぶ、Document Object Modelの略、
+//querySelectorはページに一部(HTML要素)を選ぶ関数
 
+//firefoxをクリックすると画像が変わる関数
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
@@ -18,19 +22,23 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
+  //promptは、組み込みの関数、prompt (pop-up) Windowを表示して、ユーザ入力をさせる。
+  
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    //入力データを、特別な場所(変数ではない、ファイルなどに相当)に保存する。
+    myHeading.innerHTML = 'こんにちは, ' + myName;
   }
 }
 
 if(!localStorage.getItem('name')) {
+  //以前に特別な場所(ファイルなど)保存していた名前があれば、それを使う。
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'さようなら, ' + storedName;
 }
 
 myButton.onclick = function() {
